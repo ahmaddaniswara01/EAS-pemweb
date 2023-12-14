159e757bcdb363d9c4a608d0e05bec74dcdc060e
Simply Done <afauzand388@gmail.com>
2023-12-14 15:45:20 +0700

    first commit

---
+++ 5027221057_vue/regis-vue/src/views/cekstatus.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h2>Cek Status Pendaftaran</h2>
+    <form @submit.prevent="checkStatus">
+      <label for="email">Email:</label>
+      <input type="email" v-model="email" required />
+
+      <button type="submit">Cek Status</button>
+    </form>
+    <div v-if="status !== null">
+      <p>Status Pendaftaran: {{ status }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      email: "",
+      status: null,
+    };
+  },
+  methods: {
+    checkStatus() {
+      this.status = Math.random() < 0.5 ? "Diterima" : "Ditolak";
+    },
+  },
+};
+</script>
